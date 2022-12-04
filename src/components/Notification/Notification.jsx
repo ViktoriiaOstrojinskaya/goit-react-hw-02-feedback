499cf1a0734885d124387defec4ae0f4c4ac1b72
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-12-04 11:37:23 -0500

    check

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Message } from './Notification.sryled';
+import { Message } from './Notification.styled';
 
 export const Notification = ({ message }) => <Message>{message}</Message>;
 
